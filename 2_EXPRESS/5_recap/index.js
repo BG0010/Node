d06c66b9ed8122419731c850eee212ad1c0c86d0
Vinícius Borges <borgesvini11@gmail.com>
2023-09-25 15:59:08 -0300

    recapitulação de render HTML

--- 2_EXPRESS/5_recap/index.js
+++ 2_EXPRESS/5_recap/index.js
@@ -1,10 +1,14 @@
 const express = require("express")  //importa express
+const path = require("path")
 
 const app = express()
+const caminho = path.join(__dirname, "templates")  //dirname significa pasta
 
 //mandar mensagem para porta 3000
 app.get('/', (requisicao, resposta) => {  //primeiro a rota depois o que vai receber ou enviar
-    resposta.send("Seja bem-vindo(a) ao servidor negro!") //send significa enviar
+    // resposta.send("Seja bem-vindo(a) ao servidor negro!") //send significa enviar
+    resposta.sendFile(`${caminho}/index.html`)
+
 })
 
 app.listen(3000, () => {
